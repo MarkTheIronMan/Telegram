a3e49212951a62d5c82fbf9762ebcf90be6b7fd6
MarkTheIronMan <medvedevkn@gmail.com>
2019-07-01 16:58:52 +0300

    upd

--- index.php
+++ index.php
@@ -7,13 +7,24 @@
   $text = $result["message"]["text"];
   $chat_id = $result["message"]["chat"]["id"]; 
   $name = $result["message"]["from"]["username"]; 
+  $wasStart = false;
+  $key = 11020a5d-6bfd-4b23-9b79-a24644bd3105;
+  $city = "Москва";
+  $country_name = "Russia";
+  $keyboard = [["Да"], ["Нет"]];
   if ($text == "/start") {
+  	$wasStart = true;
   	if ($name) {
       $reply = "Здравствуй, " .$name;
     }
     else {
       $reply = "Здравствуй, странник";
     }
+  }
+
+  if ($wasStart) {
+    $request = 'api.airvisual.com/v2/states?country='.$country_name.'&key='.$key.'';
+    $reply = ($request);
   } 
   
   $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply]);
