e7de316a4e32e41ceaa8deed6cc6dbe8a5c3108f
MarkTheIronMan <medvedevkn@gmail.com>
2019-07-01 14:35:33 +0300

    update

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
-  echo('hello world');
+  include('vendor/autoload.php'); 
+  use Telegram\Bot\Api; 
+ 
+  $telegram = new Api('698149481:AAFNPsmhDJ2a_dzbVFjiGCc3TgpGft-0Xk');
+  $result = $telegram -> getWebhookUpdates(); 
+  $text = $result["message"]["text"];
+  $chat_id = $result["message"]["chat"]["id"]; 
+  $name = $result["message"]["from"]["username"]; 
+  if ($text == "/start") {
+  	$reply = "Welcome, " .$name;
+  }
 ?>
