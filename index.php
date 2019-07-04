26ab2ae60aa29b1f73fe317fa8b6e9bec7978df4
MarkTheIronMan <medvedevkn@gmail.com>
2019-07-04 10:12:28 +0300

    1

--- index.php
+++ index.php
@@ -71,7 +71,7 @@
   if ($text == "/nw") {
   	$reply = RESP;
   	$mark = TRUE;
-  	$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply]);
+  	$telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply]);
   }
 /*  if (($text != "/nw") and ($mark)) {
    	  $city = $text;   
@@ -87,8 +87,16 @@
   	 $keyboard = [["Увидеть последние запросы"],["Новый город"]];
   	 $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => true ]);
      $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
-     if ("$text" == "/Увидеть последние запросы") {
+     if ("$text" == "Увидеть последние запросы") {
      	$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Как скажешь, мой повелитель"]);
+     }
+     elseif ("$text" == "Новый город") {
+     	$isNewSearch = TRUE;
+     	$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Введиете название города"]);
+     }
+     if (($isNewSearch) and ($text)) {
+     	$isNewSearch = false;
+        $telegram->sendMassage(['chat_id' => $chat_id, 'text' => getInfo($text)]);
      }
   }
 
