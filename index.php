77cce04543250f95f64d4b20bf006235ae200a3a
MarkTheIronMan <medvedevkn@gmail.com>
2019-07-04 12:51:11 +0300

    1

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
    
 
   if ($text) { 	
-  	$m = substr($text, 0, 1);
+  	$m = $text{0};
   	if ($m == "5") {
   	   $telegram->sendMassage(['chat_id' => $chat_id, 'text' => 'ну ты и пидор']);
   	} 
