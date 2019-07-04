8d03e9ba81102f06022b1d56c91bb0769ba4a6ea
MarkTheIronMan <medvedevkn@gmail.com>
2019-07-04 13:33:06 +0300

    1

--- index.php
+++ index.php
@@ -26,28 +26,15 @@
        $tempCels = 0 + $tempC;
        $tempCels = round($tempCels - 273.15); 
        $windC = $request["wind"]["speed"];
-       $str = 'Температура в '.$nameC.' составляет '.$tempCels.' градусов. Скорость ветра '.$windC.' метров в секунду.';
+       $str = 'Температура в '.$nameC.' составляет '.$tempCels.' градусов Цельсия. Скорость ветра '.$windC.' метров в секунду.';
        return $str;
     }
-
-  if (substr($text, 0, 1) === '!') {
-       $telegram->sendMassage(['chat_id' => $chat_id, 'text' => 'ну ты и пидор']);
-       $city = 'москва';/* (ltrim($text, '!'));  */
-       $reply = getInfo($city);
-       $telegram->sendMassage(['chat_id' => $chat_id, 'text' => $reply]);
-   }
-
-   if (($text) and (strpos($text, '!') === 0)) {
-       $telegram->sendMassage(['chat_id' => $chat_id, 'text' => 'ну ты и пидор']);
-   }
-
    
 
-  if ($text) { 	
-  	$m = $text[0];
-  	if ($m == "5") {
-  	   $telegram->sendMassage(['chat_id' => $chat_id, 'text' => 'ну ты и пидор']);
+  if ($text == "moscow") { 	
+  	   $telegram->sendMassage(['chat_id' => $chat_id, 'text' => getInfo($text)]);
   	} 
+  if ($text) {
     if ($text == "/start") {
       if ($name) {
         $reply = "Добро пожаловать, " .$name;
