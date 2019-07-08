9ed35f3159f0717bda34250a09063421bd87a9f8
MarkTheIronMan <medvedevkn@gmail.com>
2019-07-08 07:08:35 +0300

    1

--- index.php
+++ index.php
@@ -101,7 +101,7 @@
       $telegram->sendMessage(['chat_id' => $chat_id, 'text' => 'Введите название города']);
     }
     elseif (substr($data["city"], 0, 1) === '!') {
-      $telegram->sendMessage(['chat_id' => $chat_id, 'text' => 'test string']);
+      $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $data["city"]]);
  /*      $city = ltrim($text, '!');   
        $reply = getInfo($city);
        $telegram->sendMassage(['chat_id' => $chat_id, 'text' => $city]);
